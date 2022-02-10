b296bb0e0033a6b1729b5501e428acb9b3e277a0
shubhrad1 <shubhramail.das@gmail.com>
2022-02-11 02:25:22 +0530

    Setting adjustment

--- shubhrad1/settings.py
+++ shubhrad1/settings.py
@@ -112,9 +112,6 @@ STATIC_URL = '/static/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
